85a6b4f90ac4603f1137875a6bea212390c7a4f1
Jędrzej Runowicz <j.runowicz@tkhtechnology.com>
2022-04-28 14:23:57 +0200

    Add test to every endpoint

--- app/api_test.py
+++ app/api_test.py
@@ -19,17 +19,30 @@ update_data = {
 
 user_id = []
 class TestClass:
+
     def test_getting_user_data(self):
-        response = requests.get("http://localhost:8000/api/v1/get")
+        response = requests.get("http://localhost:8000/api/v1/users")
+        user_id.append(response.json()[-1]["py_id"])
         assert response.status_code==200
+
+
+    def test_getting_one_user_data(self):
+        response = requests.get(f"http://localhost:8000/api/v1/users/1/{user_id[0]}")
+        assert response.status_code==200
+
+
     def test_inserting_user_data(self):
-        data_post = requests.post("http://localhost:8000/api/v1/post", data=json.dumps(test_data))
-        response = requests.get("http://localhost:8000/api/v1/get")
+        data_post = requests.post("http://localhost:8000/api/v1/users", data=json.dumps(test_data))
+        response = requests.get("http://localhost:8000/api/v1/users")
         user_id.append(response.json()[-1]["py_id"])
         assert data_post.status_code==200
+    
+    
     def test_updating_user_data(self):
-        data_put = requests.put("http://localhost:8000/api/v1/put", data=json.dumps(test_data))
+        data_put = requests.put(f"http://localhost:8000/api/v1/users/1/{user_id[-1]}", data=json.dumps(test_data))
         assert data_put.status_code==200
+    
+    
     def test_deleting_user_data(self):
-        data_delete = requests.delete(f"http://localhost:8000/api/v1/delete/{user_id[0]}")
+        data_delete = requests.delete(f"http://localhost:8000/api/v1/users/1/{user_id[-1]}")
         assert data_delete.status_code==200
